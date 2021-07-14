8de9dc7f8efa4194a647258d1c92d5fafcddc092
SanCebar <r19velasco@gmail.com>
2021-07-13 21:24:27 -0700

    added an env template

--- models/books.js
+++ models/books.js
@@ -6,7 +6,6 @@ class Book {
 	//get alll book siwth specific key word
 	static async getAllBooksByyKeyword(key_word) {
 		let get_book_by_name_url = `https://www.googleapis.com/books/v1/volumes?q=${key_word}`;
-		console.log(get_book_by_name_url); //TODO: delete comment
 
 		const response = await fetch(get_book_by_name_url);
 		const responseData = await response.json();
